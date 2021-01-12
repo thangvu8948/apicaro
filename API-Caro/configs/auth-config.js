2e421c2dc0ac7396d6bcef6c1fdf0db8fed4b048
Đức Thắng <thangvu8948@gmail.com>
2021-01-13 04:39:31 +0700

    + http

--- API-Caro/configs/auth-config.js
+++ API-Caro/configs/auth-config.js
@@ -5,7 +5,7 @@ module.exports = {
     'facebookAuth': {
         'clientID': '894530304653560', // App ID c?a b?n
         'clientSecret': 'afc8dd6f25bcb6de995058384abbdcdb', // App Secret c?a b?n
-        'callbackURL': '/auth/facebook/callback'
+        'callbackURL': 'https://apicaro.herokuapp.com/auth/facebook/callback'
     },
     'googleAuth': {
         'clientID': '411712365546-e4rbjift3cqgmr37s5f8g6ebtuuisli6.apps.googleusercontent.com',
